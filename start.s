27b927ac5624b8a153e3ac38d9991b278320af2f
kritomas <Unknown>
2024-03-29 18:34:19 +0100

    More typos.

--- start.s
+++ start.s
@@ -4,7 +4,7 @@
 _start:
 	la sp, __stack_top # Set the stack pointer to the top
 	add s0, sp, zero # Set the frame pointer to the stack pointer
-	call cstart # Call cstart from cstart.c
+	call cstart # Call cstart() from cstart.c
 	j loop # Idle loop
 
 loop:	j loop # Idle loop after cstart returns
